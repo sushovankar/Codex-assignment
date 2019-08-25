1df5e23d6166d6600f9bd8c3408f3b28593ea449
Sushovan Kar <Sushovan Kar@DESKTOP-VMSKM4A>
2019-08-25 12:06:13 +0530

    Codex Assignment

---
+++ Codex-assignment/src/CB1A1Q4.java
@@ -0,0 +1,19 @@
+
+class CB1A1Q4 {
+
+	public static void main(String[] args) {
+		int a,b,c,d;
+		a=10;
+		b=19;
+		c=21;
+		d=12;
+		int x;
+		x=a>b?a:b;
+		x=x>c?x:c;
+		x=x>d?x:d;
+		System.out.println(x);
+	
+
+	}
+
+}
