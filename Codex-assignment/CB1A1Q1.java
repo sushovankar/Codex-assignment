c8d111591d7397e032f0119a2091933ab519f535
Sushovan Kar <Sushovan Kar@DESKTOP-VMSKM4A>
2019-08-25 11:25:50 +0530

    Codex Assignment

---
+++ Codex-assignment/CB1A1Q1.java
@@ -0,0 +1,12 @@
+
+public class CB1A1Q1 {
+
+	public static void main(String[] args) {
+
+	System.out.println("a."+5/2);
+	System.out.println("b."+5/2.0);
+	char ch='a';
+	System.out.println("c."+((((int)ch+1)%2)==0));
+	}
+
+}
---
+++ Codex-assignment/CB1A1Q2b.java
@@ -0,0 +1,10 @@
+
+public class CB1A1Q2b {
+
+	public static void main(String[] args) {
+	int a=8;
+	System.out.println(a++ +","+ ++a +","+ a++);
+	System.out.println(--a +","+ a +","+ a--);
+	}
+
+}
